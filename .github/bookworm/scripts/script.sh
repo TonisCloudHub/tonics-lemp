2792943bbe4d8a7efba982744af2c6e7167da9ae
Olayemi Faruq <37757164+devsrealm@users.noreply.github.com>
2024-04-01 01:09:35 +0100

    Update script.sh
    Replaced lxc with incus

--- .github/bookworm/scripts/script.sh
+++ .github/bookworm/scripts/script.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# Init incus
+sudo incus admin init --auto
+
 # Launch Instance
-lxc launch images:debian/bookworm/amd64 tonics-lemp
+sudo incus launch images:debian/bookworm/amd64 tonics-lemp
 
 # Dependencies
-lxc exec tonics-lemp -- bash -c "apt update -y && apt upgrade -y"
+sudo incus exec tonics-lemp -- bash -c "apt update -y && apt upgrade -y"
 
-lxc exec tonics-lemp -- bash -c "DEBIAN_FRONTEND=noninteractive apt install -y mariadb-server nginx php php8.2-fpm php8.2-mysql php8.2-mbstring php8.2-readline php8.2-gd  php8.2-gmp php8.2-bcmath  php8.2-zip php8.2-curl php8.2-intl php8.2-apcu"
+sudo incus exec tonics-lemp -- bash -c "DEBIAN_FRONTEND=noninteractive apt install -y mariadb-server nginx php php8.2-fpm php8.2-mysql php8.2-mbstring php8.2-readline php8.2-gd  php8.2-gmp php8.2-bcmath  php8.2-zip php8.2-curl php8.2-intl php8.2-apcu"
 
 # Setup MariaDB
-lxc exec tonics-lemp -- bash -c "mysql --user=root -sf <<EOS
+sudo incus exec tonics-lemp -- bash -c "mysql --user=root -sf <<EOS
 -- set root password
 ALTER USER root@localhost IDENTIFIED BY 'tonics_cloud';
 DELETE FROM mysql.user WHERE User='';
@@ -25,20 +28,20 @@ EOS
 "
 
 # Start Nginx
-lxc exec tonics-lemp -- bash -c "sudo nginx"
+sudo incus exec tonics-lemp -- bash -c "sudo nginx"
 
 # Clean Debian Cache
-lxc exec tonics-lemp -- bash -c "apt clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*"
+sudo incus exec tonics-lemp -- bash -c "apt clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*"
 
 # Version
-Version="Lemp__$(lxc exec tonics-lemp -- mysql -V | awk '{print $5}' | sed 's/,//')__$(lxc exec tonics-lemp -- php -v | head -n 1 | awk '{print $2}' | cut -d '-' -f 1)-PHP__$(lxc exec tonics-lemp -- nginx -v |& sed 's/nginx version: nginx\///')-Nginx"
+Version="Lemp__$(sudo incus exec tonics-lemp -- mysql -V | awk '{print $5}' | sed 's/,//')__$(sudo incus exec tonics-lemp -- php -v | head -n 1 | awk '{print $2}' | cut -d '-' -f 1)-PHP__$(sudo incus exec tonics-lemp -- nginx -v |& sed 's/nginx version: nginx\///')-Nginx"
 
 # Publish Image
-mkdir images && lxc stop tonics-lemp && lxc publish tonics-lemp --alias tonics-lemp
+mkdir images && sudo incus stop tonics-lemp && sudo incus publish tonics-lemp --alias tonics-lemp
 
 # Export Image
-lxc start tonics-lemp
-lxc image export tonics-lemp images/$Version
+sudo incus start tonics-lemp
+sudo incus image export tonics-lemp images/$Version
 
 # Image Info
-lxc image info tonics-lemp >> images/info.txt
+sudo incus image info tonics-lemp >> images/info.txt
