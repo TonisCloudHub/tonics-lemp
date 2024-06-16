832dad2835075b263823a4aff777de1f9299ea15
Olayemi.Faruq <olayemi.faruq@techsigns.de>
2024-06-16 06:32:56 +0100

    New script changes and yml update

--- .github/bookworm/scripts/script.sh
+++ .github/bookworm/scripts/script.sh
@@ -4,17 +4,18 @@
 sudo incus admin init --auto
 
 PHP_VERSION=$1
+mariaDBVersion=$2
 
 # Launch Instance
 sudo incus launch images:debian/bookworm/amd64 tonics-lemp
 
 # Dependencies
-sudo incus exec tonics-lemp -- bash -c "apt update -y && apt upgrade -y"
+sudo incus exec tonics-lemp -- bash -c "apt update -y && apt upgrade -y && apt install -y curl apt-transport-https lsb-release"
+sudo incus exec tonics-lemp -- bash -c "curl -LsS https://r.mariadb.com/downloads/mariadb_repo_setup | sudo bash -s -- --mariadb-server-version=$mariaDBVersion"
 
 if (( $(echo "$PHP_VERSION > 8.2" | bc -l) )); then
 
   # Add Ondrej's repo source and signing key along with dependencies
-  sudo incus exec tonics-lemp -- bash -c "apt install -y curl apt-transport-https lsb-release"
   sudo incus exec tonics-lemp -- bash -c  "curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg"
   sudo incus exec tonics-lemp -- bash  <<HEREDOC
   echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(sudo incus exec tonics-lemp -- lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
@@ -24,7 +25,7 @@ HEREDOC
 
 fi
 
-sudo incus exec tonics-lemp -- bash -c "DEBIAN_FRONTEND=noninteractive apt install -y mariadb-server nginx php$PHP_VERSION php$PHP_VERSION-fpm php$PHP_VERSION-{mysql,mbstring,readline,gd,gmp,bcmath,zip,curl,intl,apcu}"
+sudo incus exec tonics-lemp -- bash -c "DEBIAN_FRONTEND=noninteractive apt update -y && apt install -y mariadb-server nginx php$PHP_VERSION php$PHP_VERSION-fpm php$PHP_VERSION-{mysql,mbstring,readline,gd,gmp,bcmath,zip,curl,intl,apcu}"
 
 # Setup MariaDB
 sudo incus exec tonics-lemp -- bash -c "mysql --user=root -sf <<EOS
@@ -49,7 +50,7 @@ sudo incus exec tonics-lemp -- bash -c "sudo nginx"
 sudo incus exec tonics-lemp -- bash -c "apt clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*"
 
 # Version
-Version="Lemp__$(sudo incus exec tonics-lemp -- mysql -V | awk '{print $5}' | sed 's/,//')__$(sudo incus exec tonics-lemp -- php -v | head -n 1 | awk '{print $2}' | cut -d '-' -f 1)-PHP__$(sudo incus exec tonics-lemp -- nginx -v |& sed 's/nginx version: nginx\///')-Nginx"
+Version="Lemp__$(sudo incus exec tonics-lemp -- mariadbd --version | awk '{print $3}' | sed 's/,//')__$(sudo incus exec tonics-lemp -- php -v | head -n 1 | awk '{print $2}' | cut -d '-' -f 1)-PHP__$(sudo incus exec tonics-lemp -- nginx -v |& sed 's/nginx version: nginx\///')-Nginx"
 
 # Publish Image
 mkdir images && sudo incus stop tonics-lemp && sudo incus publish tonics-lemp --alias tonics-lemp
